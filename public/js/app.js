c85553e7f5a911a6ec60db52c432a80522bba3e3
julianamtyler <julianam.tyler@gmail.com>
2018-11-26 21:44:33 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+  var $newItemInput = $("input.new-item");
+  var $todoContainer = $(".todo-container");
+
+  $(document).on("click", "button.delete", deleteItem);
+  $(document).on("click", "button.complete", toggleComplete);
+  $(document).on("click", ".todo-item", editTodo);
+  $(document).on("keyup", ".todo-item", finishEdit);
+  $(document).on("blur", ".todo-item", cancelEdit);
+  $(document).on("submit", "#todo-form", newTodo);
+
+  var todos = [];
+
+  getTodos();
+
+  function createnew() {
+    $todoContainer.empty();
+    var newItems = [];
+    for (var i = 0; i < todos.length; i++) {
+      newItems.push(createNewRow(todos[i]));
+    }
+    $todoContainer.prepend(newItems);
+  }
+
+  function getTodos() {
+    $.get("/api/todoItems", function(data) {
+      todos = data;
+      createnew();
+    });
+  }
+
+  function deleteItem(event) {
+    event.stopPropagation();
+    var id = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/todoItems/" + id
+    }).then(getTodos);
+  }
+
+  function editTodo() {
+    var currentTodo = $(this).data("todo");
+    $(this).children().hide();
+    $(this).children("input.edit").val(currentTodo.text);
+    $(this).children("input.edit").show();
+    $(this).children("input.edit").focus();
+  }
+
+  function toggleComplete(event) {
+    event.stopPropagation();
+    var todo = $(this).parent().data("todo");
+    todo.complete = !todo.complete;
+    updateTodo(todo);
+  }
+
+  function finishEdit(event) {
+    var updatedTodo = $(this).data("todo");
+    if (event.which === 13) {
+      updatedTodo.text = $(this).children("input").val().trim();
+      $(this).blur();
+      updateTodo(updatedTodo);
+    }
+  }
+
+  function updateTodo(todo) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/todoItems",
+      data: todo
+    }).then(getTodos);
+  }
+
+
+  function cancelEdit() {
+    var currentTodo = $(this).data("todo");
+    if (currentTodo) {
+      $(this).children().hide();
+      $(this).children("input.edit").val(currentTodo.text);
+      $(this).children("span").show();
+      $(this).children("button").show();
+    }
+  }
+
+  function createNewRow(todo) {
+    var $newInput = $(
+      [
+        "<li class='todo-item'>",
+        "<button class='complete btn'>✓</button>",
+
+        "<span>",
+        todo.text,
+        "</span>",
+        "<input type='text' class='edit' style='display: none;'>",
+        "</li>"
+      ].join("")
+    );
+
+    $newInput.data("todo", todo);
+    if (todo.complete) {
+      $newInput.find("button").css("background", "green");
+      $newInput.find("span").css("text-decoration", "line-through");
+    }
+    return $newInput;
+  }
+
+  function newTodo(event) {
+    event.preventDefault();
+    var todo = {
+      text: $newItemInput.val().trim(),
+      complete: false
+    };
+
+    $.post("/api/todoItems", todo, getTodos);
+    $newItemInput.val("");
+  }
+});
---
+++ routes/api-routes.js
@@ -0,0 +1,50 @@
+
+var db = require("../models");
+
+module.exports = function(app) {
+
+  app.get("/api/todoItems", function(req, res) {
+    db.Todo.findAll({}).then(function(dbTodo) {
+      res.json(dbTodo);
+    });
+
+  });
+
+  app.post("/api/todoItems", function(req, res) {
+
+    db.Todo.create({
+      text: req.body.text,
+      complete: req.body.complete
+    }).then(function(dbTodo) {
+      res.json(dbTodo);
+    });
+
+  });
+
+  app.delete("/api/todoItems/:id", function(req, res) {
+    db.Todo.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbTodo) {
+        res.json(dbTodo);
+      });
+
+  });
+
+  app.put("/api/todoItems", function(req, res) {
+    db.Todo.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    })
+      .then(function(dbTodo) {
+        res.json(dbTodo);
+      });
+
+  });
+};
